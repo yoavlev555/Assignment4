4ac7c86b33cf6ff27f1e1883e7f28fef4a58d759
Yoav Levin <y_levin@apple.com>
2025-06-25 09:56:35 +0300

    implement with bug with moris

--- ex4.pl
+++ ex4.pl
@@ -37,13 +37,25 @@ book(the_well_at_the_worlds_end, m, f, s(s(s(s(zero))))).
 book(the_hobbit, t, f, s(s(s(zero)))).
 book(the_lord_of_the_rings, t, f, s(s(s(s(s(s(zero))))))).
 
-% You can add more facts.
+% You can add more facts
+bigger_then(A, zero, A).
+bigger_then(s(A), s(B), s(A)) :- bigger_then(A, B, A).
 
+
+%list_bigger(A, [], A)
+%list_bigger([T1 | S1], [T2 | S2], [T1 | S1]) :- list_bigger(S1, S2, S1)
+
+%length_of_books_by_author(AuthorName, Lengths) :- author(AuthorID, AuthorName), findall(T, book(BookName, AuthorID, GenereID, T), Lengths).
 
 % Signature: max_list(Lst, Max)/2
 % Purpose: true if Max is the maximum church number in Lst, false if Lst is emoty.
-
-
+max_list([], false).
+max_list([T], T).
+max_list([A, B], A) :- bigger_then(A, B, A), !.
+max_list([_, B], B).
+max_list([H | T], Max) :-
+    max_list(T, MaxT),
+    max_list([H, MaxT], Max).
 
 
 
@@ -52,9 +64,10 @@ book(the_lord_of_the_rings, t, f, s(s(s(s(s(s(zero))))))).
 
 % Signature: author_of_genre(GenreName, AuthorName)/2
 % Purpose: true if an author by the name AuthorName has written a book belonging to the genre named GenreName.
-
-
-
+author_of_genre(GenreName, AuthorName) :-
+    author(AuthorID, AuthorName),
+    genre(GenreID, GenreName),
+    book(_, AuthorID, GenreID, _).
 
 
 
@@ -62,3 +75,9 @@ book(the_lord_of_the_rings, t, f, s(s(s(s(s(s(zero))))))).
 
 % Signature: longest_book(AuthorName, BookName)/2
 % Purpose: true if the longest book that an author by the name AuthorName has written is titled BookName.
+longest_book(AuthorName, BookName) :-
+    author(AuthorID, AuthorName),
+    findall((Title, Length), book(Title, AuthorID, _, Length), Pairs),
+    findall(Length, member((_, Length), Pairs), Lengths),
+    max_list(Lengths, Max),
+    member((BookName, Max), Pairs).
